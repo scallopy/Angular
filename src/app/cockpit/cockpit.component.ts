29db7765903a8f6436aebbd7ef1b215e85e0d4f1
scallopy <scallopy@abv.bg>
2019-05-03 09:07:16 +0300

    Update tar to ^4.8.8

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
   newServerName = '';
   newServerContent = '';
+  newBlueprintName = '';
+  newBlueprintContent = '';
 
   constructor() { }
 
@@ -15,18 +19,16 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-  //this.serverElements.push({
-  //  type: 'server',
-  //  name: this.newServerName,
-  //  content: this.newServerContent
-  // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
-  onAddBlueprint() {  
-  // this.serverElements.push({
-  // type: 'blueprint',
-  // name: this.newServerName,
-  // content: this.newServerContent
-  // });
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      blueprintName: this.newBlueprintName,
+      blueprintContent: this.newBlueprintContent
+    });
   }
 }
